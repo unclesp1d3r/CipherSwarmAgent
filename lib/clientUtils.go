6bf96ab084ea8f97cf07591c16eceba70e136261
UncleSp1d3r <unclespider@protonmail.com>
2024-07-01 17:52:39 -0400

    refactor: Moved utility code out of agentClient.go

--- lib/clientUtils.go
+++ lib/clientUtils.go
@@ -1,7 +1,17 @@
 package lib
 
 import (
+	"context"
+	"encoding/hex"
 	"errors"
+	"github.com/duke-git/lancet/cryptor"
+	"github.com/duke-git/lancet/v2/validator"
+	"github.com/hashicorp/go-getter"
+	"github.com/unclesp1d3r/cipherswarm-agent-sdk-go/models/components"
+	"github.com/unclesp1d3r/cipherswarmagent/lib/utils"
+	"io"
+	"net/http"
+	url2 "net/url"
 	"os"
 	"path"
 
@@ -16,13 +26,13 @@ import (
 	"github.com/unclesp1d3r/cipherswarmagent/lib/arch"
 )
 
-// GetCurrentHashcatVersion retrieves the current version of Hashcat.
+// getCurrentHashcatVersion retrieves the current version of Hashcat.
 // It checks if the Hashcat directory exists and then uses the arch.GetHashcatVersion
 // function to get the version from the specified path.
 // If the Hashcat directory does not exist, it returns "0.0.0" and an error.
 // If there is an error retrieving the version, it also returns "0.0.0" and the error.
 // Otherwise, it returns the Hashcat version and no error.
-func GetCurrentHashcatVersion() (string, error) {
+func getCurrentHashcatVersion() (string, error) {
 	// Check where the hashcat binary should be
 	hashcatExists := fileutil.IsExist(viper.GetString("hashcat_path"))
 	if !hashcatExists {
@@ -133,4 +143,190 @@ func CreateDataDirs() error {
 		}
 	}
 	return nil
+}
+
+func downloadHashList(attack *components.Attack) error {
+	// Download the hashlist
+	hashlistPath := path.Join(shared.State.HashlistPath, convertor.ToString(attack.GetHashListID())+".txt")
+	shared.Logger.Debug("Downloading hashlist", "url", attack.GetHashListURL(), "path", hashlistPath)
+	// We should always download the hashlist, even if it already exists
+	// This is because the hashlist may have been updated on the server
+	if fileutil.IsExist(hashlistPath) {
+		err := os.Remove(hashlistPath)
+		if err != nil {
+			shared.Logger.Error("Error removing old hashlist", "error", err)
+			SendAgentError(err.Error(), nil, components.SeverityCritical)
+			return err
+		}
+	}
+
+	response, err := SdkClient.Attacks.GetHashList(Context, attack.ID)
+	if err != nil {
+		shared.Logger.Error("Error downloading hashlist from the CipherSwarm API", "error", err)
+		SendAgentError(err.Error(), nil, components.SeverityCritical)
+		return err
+	}
+
+	if response.StatusCode == http.StatusOK {
+		if response.ResponseStream != nil {
+			f, err := os.Create(hashlistPath)
+			if err != nil {
+				shared.Logger.Error("Error creating hashlist file", "error", err)
+				SendAgentError(err.Error(), nil, components.SeverityCritical)
+				return err
+			}
+			defer f.Close()
+			_, err = io.Copy(f, response.ResponseStream)
+			if err != nil {
+				shared.Logger.Error("Error writing hashlist file", "error", err)
+				SendAgentError(err.Error(), nil, components.SeverityCritical)
+				return err
+			}
+			shared.Logger.Debug("Downloaded hashlist", "path", hashlistPath)
+			hashSize, _ := fileutil.FileSize(hashlistPath)
+			if hashSize == 0 {
+				shared.Logger.Error("Downloaded hashlist is empty", "path", hashlistPath)
+				SendAgentError("Downloaded hashlist is empty", nil, components.SeverityCritical)
+				return errors.New("downloaded hashlist is empty, probably completed task")
+			}
+		}
+	} else {
+		shared.Logger.Error("Error downloading hashlist", "response", response.RawResponse.Status)
+		return errors.New("failed to download hashlist")
+	}
+	return nil
+}
+
+// downloadFile downloads a file from the specified URL and saves it to the given path.
+// If a checksum is provided, it verifies the downloaded file against the checksum.
+// If the file already exists and the checksum matches, it returns without downloading again.
+// If the file already exists but the checksum does not match, it deletes the existing file and downloads a new one.
+// It displays the progress of the download and completion status.
+// Parameters:
+//   - url: The URL of the file to download.
+//   - path: The path where the downloaded file will be saved.
+//   - checksum: The checksum to verify the downloaded file against (optional).
+//
+// Returns:
+//   - error: An error if any occurred during the download or verification process, or nil if successful.
+func downloadFile(url string, path string, checksum string) error {
+	if !validator.IsUrl(url) {
+		shared.Logger.Error("Invalid URL", "url", url)
+		return errors.New("invalid URL")
+	}
+
+	if fileutil.IsExist(path) {
+		if strutil.IsNotBlank(checksum) {
+			fileChecksum, err := cryptor.Md5File(path)
+			if err != nil {
+				return err
+			}
+			if fileChecksum == checksum {
+				shared.Logger.Info("Download already exists", "path", path)
+				return nil
+			}
+			shared.Logger.Warn("Checksums do not match", "path", path, "url_checksum", checksum, "file_checksum", fileChecksum)
+			SendAgentError("Resource "+path+" exists, but checksums do not match", nil, components.SeverityInfo)
+			err = os.Remove(path)
+			if err != nil {
+				SendAgentError(err.Error(), nil, components.SeverityMajor)
+				return err
+			}
+		}
+	}
+	DisplayDownloadFile(url, path)
+	cwd, err := os.Getwd()
+	if err != nil {
+		shared.Logger.Error("Error getting current working directory: ", "error", err)
+	}
+
+	if strutil.IsNotBlank(checksum) {
+		urlA, err := url2.Parse(url)
+		if err != nil {
+			shared.Logger.Error("Error parsing URL: ", "error", err)
+			return err
+		}
+		values := urlA.Query()
+		values.Add("checksum", checksum)
+		urlA.RawQuery = values.Encode()
+		url = urlA.String()
+	}
+
+	client := &getter.Client{
+		Ctx:      context.Background(),
+		Dst:      path,
+		Src:      url,
+		Pwd:      cwd,
+		Insecure: true,
+		Mode:     getter.ClientModeFile,
+	}
+
+	_ = client.Configure(
+		getter.WithProgress(utils.DefaultProgressBar),
+		getter.WithUmask(os.FileMode(0o022)),
+	)
+
+	if err := client.Get(); err != nil {
+		shared.Logger.Debug("Error downloading file: ", "error", err)
+		return err
+	}
+	DisplayDownloadFileComplete(url, path)
+	return nil
+}
+
+// extractHashcatArchive extracts a hashcat archive to the specified location.
+// It removes the old hashcat backup directory, moves the old hashcat directory to a backup location,
+// and then extracts the new hashcat directory using the 7z command.
+// It returns the path of the extracted hashcat directory and any error encountered during the process.
+func extractHashcatArchive(newArchivePath string) (string, error) {
+	hashcatDirectory := path.Join(shared.State.CrackersPath, "hashcat")
+	hashcatBackupDirectory := hashcatDirectory + "_old"
+	// Get rid of the old hashcat backup directory
+	err := os.RemoveAll(hashcatBackupDirectory)
+	if err != nil && !os.IsNotExist(err) {
+		shared.Logger.Error("Error removing old hashcat directory: ", "error", err)
+		SendAgentError(err.Error(), nil, components.SeverityCritical)
+		return "", err // Don't continue if we can't remove the old directory
+	}
+
+	// Move the old hashcat directory to a backup location
+	err = os.Rename(hashcatDirectory, hashcatBackupDirectory)
+	if err != nil && !os.IsNotExist(err) {
+		shared.Logger.Error("Error moving old hashcat directory: ", "error", err)
+		SendAgentError(err.Error(), nil, components.SeverityCritical)
+		return "", err // Don't continue if we can't move the old directory
+	}
+
+	// Extract the new hashcat directory using the 7z command
+	err = arch.Extract7z(newArchivePath, shared.State.CrackersPath)
+	if err != nil {
+		shared.Logger.Error("Error extracting file: ", "error", err)
+		SendAgentError(err.Error(), nil, components.SeverityCritical)
+		return "", err // Don't continue if we can't extract the file
+	}
+	return hashcatDirectory, err
+}
+
+// moveArchiveFile moves the archive file from the temporary path to a new path.
+// It takes the temporary archive path as input and returns the new archive path and an error (if any).
+// The function renames the file using the os.Rename function and logs any errors encountered.
+// It also logs the old and new paths of the file after the move operation.
+func moveArchiveFile(tempArchivePath string) (string, error) {
+	newArchivePath := path.Join(shared.State.CrackersPath, "hashcat.7z")
+	err := os.Rename(tempArchivePath, newArchivePath)
+	if err != nil {
+		shared.Logger.Error("Error moving file: ", err)
+		return "", err
+	}
+	shared.Logger.Debug("Moved file", "old_path", tempArchivePath, "new_path", newArchivePath)
+	return newArchivePath, err
+}
+
+func base64ToHex(base64 string) string {
+	if strutil.IsBlank(base64) {
+		return ""
+	}
+	str := cryptor.Base64StdDecode(base64)
+	hx := hex.EncodeToString([]byte(str))
+	return hx
 }
