8f263a8c6ef4fcf52ca733f213c68337dfc2023e
UncleSp1d3r <unclesp1d3r@evilbitlabs.io>
2026-02-26 22:42:58 -0500

    refactor: simplify file checksum validation and improve function naming
    Signed-off-by: UncleSp1d3r <unclesp1d3r@evilbitlabs.io>

--- lib/downloader/downloader.go
+++ lib/downloader/downloader.go
@@ -8,7 +8,6 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
-	"hash"
 	"io"
 	"net/http"
 	"net/url"
@@ -71,14 +70,14 @@ func FileExistsAndValid(filePath, checksum string) bool {
 		return true
 	}
 
-	digest, err := fileChecksum(filePath, md5.New()) //nolint:gosec // G401 - MD5 for integrity
+	fileChecksum, err := fileMD5(filePath)
 	if err != nil {
 		agentstate.Logger.Error("Error calculating file checksum", "path", filePath, "error", err)
 
 		return false
 	}
 
-	if digest == checksum {
+	if fileChecksum == checksum {
 		return true
 	}
 
@@ -89,7 +88,7 @@ func FileExistsAndValid(filePath, checksum string) bool {
 		"url_checksum",
 		checksum,
 		"file_checksum",
-		digest,
+		fileChecksum,
 	)
 
 	if err := os.Remove(filePath); err != nil {
@@ -145,29 +144,8 @@ func downloadAndVerifyFile(ctx context.Context, fileURL, filePath, checksum stri
 		return err
 	}
 
-	if strings.TrimSpace(checksum) != "" {
-		digest, err := fileChecksum(filePath, md5.New()) //nolint:gosec // G401 - MD5 for integrity
-		if err != nil {
-			return fmt.Errorf("error calculating downloaded file checksum: %w", err)
-		}
-
-		if digest != checksum {
-			agentstate.Logger.Warn(
-				"Checksums do not match",
-				"path",
-				filePath,
-				"url_checksum",
-				checksum,
-				"file_checksum",
-				digest,
-			)
-
-			if err := os.Remove(filePath); err != nil {
-				agentstate.Logger.Error("Error removing file with mismatched checksum", "path", filePath, "error", err)
-			}
-
-			return errors.New("downloaded file checksum does not match")
-		}
+	if strings.TrimSpace(checksum) != "" && !FileExistsAndValid(filePath, checksum) {
+		return errors.New("downloaded file checksum does not match")
 	}
 
 	return nil
@@ -217,7 +195,6 @@ func appendChecksumToURL(rawURL, checksum string) (string, error) {
 	}
 
 	q := u.Query()
-	// TODO(#118): migrate to SHA-256 once CipherSwarm server API supports it
 	q.Set("checksum", "md5:"+checksum)
 	u.RawQuery = q.Encode()
 
@@ -327,9 +304,8 @@ func writeResponseToFile(responseStream io.Reader, filePath string) error {
 	return nil
 }
 
-// TODO(#118): migrate to SHA-256 once CipherSwarm server API supports it
-// fileChecksum computes the hex digest of the file at the given path using the provided hash.
-func fileChecksum(filePath string, h hash.Hash) (string, error) {
+// fileMD5 computes the MD5 hex digest of the file at the given path.
+func fileMD5(filePath string) (string, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
 		return "", err
@@ -342,6 +318,7 @@ func fileChecksum(filePath string, h hash.Hash) (string, error) {
 		}
 	}()
 
+	h := md5.New() //nolint:gosec // G401 - MD5 used for file integrity check, not security
 	if _, err := io.Copy(h, f); err != nil {
 		return "", err
 	}
